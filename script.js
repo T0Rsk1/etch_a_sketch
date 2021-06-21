47229535fdb56a7881be9eae562973757e00b8f2
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-06-21 19:13:40 -0400

    Created randColor function

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const container = document.querySelector('#container');
 const maxDim = 800;
+let selec = 0;
 
-function createGrid(r, c){
-    for(let i=0;i<r;i++){
+function createGrid(x){
+    let dim = maxDim/x;
+
+    for(let i=0;i<x;i++){
         let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for(let j=0;j<c;j++){
+        for(let j=0;j<x;j++){
             let col = document.createElement('div')
-            col.classList.add('col');
-            col.style.height = `${maxDim/r}px`;
-            col.style.width = `${maxDim/c}px`;
-            col.addEventListener('mouseover', () => onHover(col));
+            /*col.classList.add('col');*/
+            col.style.height = `${dim}px`;
+            col.style.width = `${dim}px`;
+            col.addEventListener('mouseover', () => onHover(selec, col));
             row.appendChild(col);
         }
     }
 }
 
-function onHover(sqr){
-    sqr.classList.add('color-square');
+function onHover(selec, sqr){
+    if(selec === 0) sqr.style.background = 'black';
+    else if(selec === 1) sqr.style.background = randColor();
 }
 
-createGrid(100, 100);
+function randColor(){
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+selec = 1;
+createGrid(100);
 
