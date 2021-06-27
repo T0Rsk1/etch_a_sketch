5dbeb1df562966c4d222189586f58736f9ae2bcf
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-06-27 17:12:20 -0400

    Updated code Add color to background and text Update darken()

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function createGrid(x){
         col.classList.add('col');
         grid.appendChild(col);
         for(let j=0;j<x;j++){
-            let row = document.createElement('div')
-            row.classList.add('row');
+            let row = document.createElement('div');
+            row.style.background = 'rgb(200, 200, 200)';
             row.style.height = dim + 'px';
             row.style.width = dim + 'px';
             row.addEventListener('mouseover', () => onHover(select, row));
@@ -26,16 +26,15 @@ function createGrid(x){
 
 function onHover(sel, sqr){
     if(sel === 'clr') sqr.style.background = randColor();
-    else if(sel === 'drk') sqr.style.background = darken();
-    else sqr.style.background = 'black';
-    
+    else if(sel === 'drk') sqr.style.background = darken(sqr.style.background);
+    else sqr.style.background = 'rgb(0, 0, 0)'; 
 }
 
 function randColor(){
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function darken(){
+function ld(){
     if(!rev){
         color -= 25.5;
         if(color === 0) rev = true;
@@ -45,6 +44,14 @@ function darken(){
     }
 
     return `rgb(${color}, ${color}, ${color})`;
+}
+
+function darken(sqr){
+    let rgb = sqr.substring(4, sqr.length-1).split(', ');
+    for(let i=0;i<rgb.length;i++){
+        rgb[i] -= 20;    
+    }
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 }
 
 function getIn(){
